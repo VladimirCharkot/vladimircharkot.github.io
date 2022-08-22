8ad03d60e68324fffb7869396adb5345be94a1dd
VladimirCharkot <vlad.chk@gmail.com>
2022-08-22 17:29:09 -0300

    Fushhh

--- README.md
+++
@@ -1,7 +0,0 @@
-# profevladi.com
-Miniapps para clases de informática en el nivel primario
-
-1. Dibubots
-2. Árboles
-3. Cifrado César
-4. Variables
--- musica.js
+++
@@ -1,7 +0,0 @@
-city_music = new Audio('kandaiki.mp3');
-city_music.loop = true;
-
-function switch_musica(){
-    if (document.querySelector("#musica_switch").checked) city_music.play();
-    else city_music.pause();
-}
